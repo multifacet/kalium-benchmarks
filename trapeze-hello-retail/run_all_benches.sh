113e1e3f3a1cec42cfbba3c932940bc0d100c3d2
Deepak Sirone Jegan <dsirone@cs.wisc.edu>
2023-06-19 18:07:07 -0500

    Modify testing scripts

---
+++ trapeze-hello-retail/run_all_benches.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p results_trapeze_$1
+
+pushd ./productCatalogApi
+./run_bench.sh $1 $(pwd)/results_trapeze_$1
+popd
+
+pushd ./product-photos
+./run_bench.sh $1 $(pwd)/results_trapeze_$1
+popd
+
+pushd ./product-purchase
+./run_bench.sh $1 $(pwd)/results_trapeze_$1
+popd
+
+
--- vanilla-hello-retail/product-purchase/run_test.sh
+++ vanilla-hello-retail/product-purchase/run_test.sh
@@ -8,24 +8,28 @@ set -ex
 pushd ./1.authenticate/
 ./run_bench.sh 20 $1_20_authenticate
 ../stats.py $1_20_authenticate > $1_20_authenticate_results
+cp $1_20_authenticate_results $2
 popd
 
 # 2.getPrice
 pushd ./2.getPrice/
 ./run_bench.sh 20 $1_20_getPrice
 ../stats.py $1_20_getPrice > $1_20_getPrice_results
+cp $1_20_getPrice_results $2
 popd
 
 # 3.authorize-cc
 pushd ./3.authorize-cc/
 ./run_bench.sh 20 $1_20_authorize-cc
 ../stats.py $1_20_authorize-cc > $1_20_authorize-cc_results
+cp $1_20_authorize-cc_results $2
 popd
 
 # 4.publish
 pushd ./4.publish/
 ./run_bench.sh 20 $1_20_publish
 ../stats.py $1_20_publish > $1_20_publish_results
+cp $1_20_publish_results $2
 popd
 
 ./launch_functions.sh remove
