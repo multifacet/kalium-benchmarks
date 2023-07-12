e8338ddc30d7ca80bad9d43e992508274b70fe1e
Deepak Sirone <dsirone@cs.wisc.edu>
2023-07-12 08:29:43 -0500

    Changes for deployment

--- vanilla-hello-retail/run_all_benches.sh
+++ vanilla-hello-retail/run_all_benches.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 mkdir -p results_$1
+dir=$(pwd)/results_$1
 
 pushd ./productCatalogApi
-./run_bench.sh $1 $(pwd)/results_$1
+./run_bench.sh $1 $dir
 popd
 
 pushd ./product-photos
-./run_bench.sh $1 $(pwd)/results_$1
+./run_bench.sh $1 $dir
 popd
 
 pushd ./product-purchase
-./run_bench.sh $1 $(pwd)/results_$1
+./run_bench.sh $1 $dir
 popd
 
 
